079a8823269473712f514a7b0c28649ff0addcae
Kyle Spraggs <theman@spiffyjr.me>
2013-05-29 12:18:46 -0500

    Added initializers.

--- src/SpiffyDatatables/Manager.php
+++ src/SpiffyDatatables/Manager.php
@@ -2,13 +2,38 @@
 
 namespace SpiffyDatatables;
 
+use Zend\ServiceManager\ConfigInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
 use Zend\ServiceManager\ServiceManager;
 
 /**
  * Class Manager
  * @package SpiffyDatatables
  */
-class Manager extends ServiceManager
+class Manager extends ServiceManager implements ServiceLocatorAwareInterface
 {
+    use ServiceLocatorAwareTrait;
 
+    /**
+     * @param ConfigInterface $config
+     */
+    public function __construct(ConfigInterface $config = null)
+    {
+        if ($config) {
+            $config->configureServiceManager($this);
+        }
+
+        $this->addInitializer(function($object) {
+            if ($object instanceof ServiceLocatorAwareInterface) {
+                $object->setServiceLocator($this->getServiceLocator());
+            }
+        });
+
+        $this->addInitializer(function($object) {
+            if (method_exists($object, 'init')) {
+                $object->init();
+            }
+        });
+    }
 }
